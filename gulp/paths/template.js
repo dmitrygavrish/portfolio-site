1d09b589825cb72efeddf59f28c78d1d9c7baa3f
dmitrygavrish <dmitrygavrish@gmail.com>
2016-06-14 14:56:55 +0300

    all jade

--- gulp/paths/template.js
+++ gulp/paths/template.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = [
-  './source/template/**/*.jade'
+  './source/template/index.jade',
+  './source/template/about/about.jade',
+  './source/template/admin/admin.jade',
+  './source/template/blog/blog.jade',
+  './source/template/works/works.jade'
 ];
