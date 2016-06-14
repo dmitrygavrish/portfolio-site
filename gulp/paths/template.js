f059a4ea988bd673950668b9ba3d4c7c54624caf
dmitrygavrish <dmitrygavrish@gmail.com>
2016-06-14 23:59:16 +0300

    extend on, started re-naming

--- gulp/paths/template.js
+++ gulp/paths/template.js
@@ -1,9 +1,5 @@
 'use strict';
 
 module.exports = [
-  './source/template/index.jade',
-  './source/template/about/about.jade',
-  './source/template/admin/admin.jade',
-  './source/template/blog/blog.jade',
-  './source/template/works/works.jade'
+  './source/template/pages/*.jade'
 ];
