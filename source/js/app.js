78fee33cb2093e082e67cb0fe479ac1d969052b3
dmitrygavrish <dmitrygavrish@gmail.com>
2016-06-16 13:04:20 +0300

    16.06.2016

--- source/js/app.js
+++ source/js/app.js
@@ -3,42 +3,56 @@ var toggle = 'off';
 
 $('.authorize-button__link').on('click', function() {
   $('.welcome-plate').removeClass('rotate3d-back').addClass('rotate3d');
-  $('.authorize-plate').removeClass('rotate3d-reverse').addClass('rotate3d-back');
+  $('.authorize-plate').removeClass('rotate3d-reverse').addClass('rotate3d-back').css({'display':'block'});
+  $('.authorize-button').fadeOut(500);
   setTimeout(function(){
-    $('.welcome-top').css({
-      'opacity':'0'
-    });
-    $('.plate-menu').css({
+    $('.welcome-plate').css({
       'opacity':'0'
     });
     $('.authorize-button').css({
-      'opacity':'0'
+      'visibility':'hidden'
     });
     $('.authorize-plate').css({
       'opacity':'1'
     });
-    toggle = 'on';
   }, 500);
+  setTimeout(function() {
+    toggle = 'on';
+    $('.welcome-plate').css({
+      'transform':'rotate3d(0, 180, 0, 180deg)'
+    });
+    $('.authorize-plate').css({
+      'transform':'rotate3d(0, 0, 0, 0deg)'
+    });
+  }, 1000)
 });
 
 $(document).on('click', (function (event) {
-  if ($(event.target).closest('.authorize-plate').length == 0 && toggle == 'on') {
+  var target = $(event.target);
+  if ((target.closest('.authorize-plate').length == 0 || target.is('#return')) && toggle == 'on') {
+    toggle = 'off';
     $('.welcome-plate').removeClass('rotate3d').addClass('rotate3d-back');
     $('.authorize-plate').removeClass('rotate3d-back').addClass('rotate3d-reverse');
     setTimeout(function(){
-      $('.welcome-top').css({
+      $('.welcome-plate').css({
         'opacity':'1'
       });
-      $('.plate-menu').css({
-        'opacity':'1'
-      });
-      $('.authorize-button').css({
-        'opacity':'1'
+      $('.authorize-button').fadeIn(500).css({
+        'visibility':'visible'
       });
       $('.authorize-plate').css({
-        'opacity':'0'
+        'opacity':'0',
+        'display':'none'
       });
-      toggle = 'off';
     }, 500);
+    setTimeout(function() {
+      $('.welcome-plate').css({
+        'transform':'rotate3d(0, 0, 0, 0deg)'
+      });
+      $('.authorize-plate').css({
+        'transform':'rotate3d(0, -180, 0, -180deg)'
+      });
+    }, 1000)
   }
 }));
+
